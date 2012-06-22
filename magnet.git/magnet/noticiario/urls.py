9fc10ceefb3869492897c43f0dab0aefe2470637
Andrew Santos <andrew.santos@bsgi.org.br>
2012-06-22 18:16:18 -0300

    primeiro template iniciado

---
+++ magnet.git/magnet/noticiario/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+from .views import HomeView
+
+urlpatterns = patterns('',
+    url(r'^$', HomeView.as_view(), name='home'),
+)
