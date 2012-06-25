9fa8aeb082e57b841615b01e5b5aaea5b83b1cbc
Andrew Santos <andrew.santos@bsgi.org.br>
2012-06-25 17:48:14 -0300

    capas de canais funcionando

--- magnet.git/magnet/noticiario/urls.py
+++ magnet.git/magnet/noticiario/urls.py
@@ -8,5 +8,6 @@ from .views import Noticia
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'(?P<secao>\w+)/$', HomeView.as_view(), name='capa-secao'),
     url(r'^bits/(?P<pk>\d+)$', DetailView.as_view(model=Noticia), name='noticia-detalhe'),
 )
